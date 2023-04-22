9c9602e8a0607bb9c850c2a84c57eab4f0e34c5c
Chris Zhu <chriszhu@Chriss-MacBook-Pro.local>
2023-04-21 23:40:20 -0700

    got everythign working i production!

--- app/browsergpt/storage.py
+++ app/browsergpt/storage.py
@@ -1,10 +1,11 @@
 import os
+from sqlalchemy import create_engine
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
+SQLALCHEMY_DATABASE_URI = os.environ.get("SQLALCHEMY_DATABASE_URI")
 db = SQLAlchemy()
-
 def initialize_db(app, db):
-  app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("SQLALCHEMY_DATABASE_URI")
+  app.config["SQLALCHEMY_DATABASE_URI"] = SQLALCHEMY_DATABASE_URI
   db.init_app(app)
   Migrate(app, db)
