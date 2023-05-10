108c88739c065ade378437052b9c0edb468bed6e
Chris Zhu <chriszhu@Chriss-MacBook-Pro.local>
2023-05-10 16:13:34 -0700

    loading screen

--- frontend/src/pages/Popup/components/Loading.jsx
+++ frontend/src/pages/Popup/components/Loading.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import ClipLoader from "react-spinners/ClipLoader";
+
 
 const Loading = () => {
   return (
-    <div className="loading">
-      <h4>Loading...</h4>
+    <div className="loading d-flex" style={{justifyContent: "center", alignItems: "center", height: "100vh"}}>
+      <ClipLoader
+        color="#2980b9"
+        size={50}
+        aria-label="Loading Spinner"
+        data-testid="loader"
+      />
     </div>
   );
 };
